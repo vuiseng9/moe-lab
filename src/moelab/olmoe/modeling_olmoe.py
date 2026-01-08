87fcfba573e2638956825f32f492bf187472bf88
Vui Seng Chua <vuiseng9@gmail.com>
2026-01-08 14:22:48 -0800

    Implement shared experts in moelab_olmoe via monkey patching

--- src/moelab/olmoe/modeling_olmoe.py
+++ src/moelab/olmoe/modeling_olmoe.py
@@ -1,6 +1,6 @@
 from transformers.cache_utils import Cache
 from transformers.models.olmoe.configuration_olmoe import OlmoeConfig
-from transformers.models.olmoe.modeling_olmoe import OlmoeForCausalLM, load_balancing_loss_func
+from transformers.models.olmoe.modeling_olmoe import OlmoeForCausalLM, OlmoeMLP, load_balancing_loss_func
 from typing import Optional, Union
 from transformers.modeling_outputs import MoeCausalLMOutputWithPast
 import torch
@@ -12,6 +12,7 @@ import warnings
 class MoelabOlmoeConfig(OlmoeConfig):
     model_type = "moelab_olmoe"
     keys_to_ignore_at_inference = ["past_key_values", "aux_loss", "router_logits"]
+    num_shared_experts: int = 0
 
     def __init__(self, enable_lbloss: bool = False, **kwargs):
         # Default output_router_logits to True
@@ -32,6 +33,23 @@ class MoelabOlmoeConfig(OlmoeConfig):
 # we use added enable_lbloss flag to control whether to compute load balancing loss
 class MoelabOlmoeForCausalLM(OlmoeForCausalLM):
     config_class = MoelabOlmoeConfig
+
+    def post_init(self):
+        super().post_init()
+        self.monkey_patch()
+    
+    def monkey_patch(self):
+        # append shared experts to OlmoeSparseMoeBlock
+        for blk in self.model.layers:
+            moe = blk.mlp
+            moe.num_shared_experts = self.config.num_shared_experts
+            
+            if self.config.num_shared_experts > 0:
+                moe.shared = torch.nn.ModuleList([OlmoeMLP(self.config) for _ in range(self.config.num_shared_experts)])
+
+        # monkey patch OlmoeSparseMoeBlock forward to shared_expert_forward
+        MoeClass = type(moe)
+        MoeClass.forward = shared_expert_forward
 
     def forward(
         self,
@@ -142,3 +160,45 @@ class MoelabOlmoeForCausalLM(OlmoeForCausalLM):
             attentions=outputs.attentions,
             router_logits=outputs.router_logits,
         )
+
+def shared_expert_forward(self, hidden_states: torch.Tensor) -> torch.Tensor:
+    batch_size, sequence_length, hidden_dim = hidden_states.shape
+    hidden_states = hidden_states.view(-1, hidden_dim)
+    # router_logits: (batch * sequence_length, n_experts)
+    router_logits = self.gate(hidden_states)
+
+    routing_weights = torch.nn.functional.softmax(router_logits, dim=1, dtype=torch.float)
+    routing_weights, selected_experts = torch.topk(routing_weights, self.top_k, dim=-1)
+    if self.norm_topk_prob:
+        routing_weights /= routing_weights.sum(dim=-1, keepdim=True)
+    # we cast back to the input dtype
+    routing_weights = routing_weights.to(hidden_states.dtype)
+
+    final_hidden_states = torch.zeros(
+        (batch_size * sequence_length, hidden_dim), dtype=hidden_states.dtype, device=hidden_states.device
+    )
+
+    # One hot encode the selected experts to create an expert mask
+    # this will be used to easily index which expert is going to be selected
+    expert_mask = torch.nn.functional.one_hot(selected_experts, num_classes=self.num_experts).permute(2, 1, 0)
+
+    # Loop over all available experts in the model and perform the computation on each expert
+    for expert_idx in range(self.num_experts):
+        expert_layer = self.experts[expert_idx]
+        idx, top_x = torch.where(expert_mask[expert_idx])
+
+        # Index the correct hidden states and compute the expert hidden state for
+        # the current expert. We need to make sure to multiply the output hidden
+        # states by `routing_weights` on the corresponding tokens (top-1 and top-2)
+        current_state = hidden_states[None, top_x].reshape(-1, hidden_dim)
+        current_hidden_states = expert_layer(current_state) * routing_weights[top_x, idx, None]
+
+        # However `index_add_` only support torch tensors for indexing so we'll use
+        # the `top_x` tensor here.
+        final_hidden_states.index_add_(0, top_x, current_hidden_states.to(hidden_states.dtype))
+
+    for s in range(self.num_shared_experts):
+        final_hidden_states += self.shared[s](hidden_states)
+
+    final_hidden_states = final_hidden_states.reshape(batch_size, sequence_length, hidden_dim)
+    return final_hidden_states, router_logits
