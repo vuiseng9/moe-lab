c240c0c1ae412d480843f3b4bfc07649c0c22bfa
Vui Seng Chua <vuiseng9@gmail.com>
2026-02-13 10:17:31 -0800

    Re-enabling expert capacity

--- src/moelab/moedl/modeling_moedl.py
+++ src/moelab/moedl/modeling_moedl.py
@@ -227,87 +227,98 @@ class MoeBlk(nn.Module):
             expert_capacity = int( (T*K/E) * self.capacity_factor )
             assert expert_capacity > 0, "expert_capacity must be positive. capacity_factor too small?"
 
-        assert expert_capacity is None, "Temporarily disable expert capacity to focus on grouped gemm optimization. Will re-enable in future."
-        
-        # = dispatch ================
-        # k_ids [T, K] contextually token to k-activated expert ids
-        expanded_k_ids = k_ids.view(-1) # k-expanded for ease of understanding, it is flat too
-        
-        # torch.sort returns sorted values and indices to original positions
-        sorted_expanded_ids, expanded_ids_to_sorted = torch.sort(expanded_k_ids, stable=True)
+            # = expert_capacity handling ================
+            # loop over experts
+            tok_ids_by_e = []
+            which_k_by_e = []
+            for eid in range(E):
+                # tok_ids: indices of tokens to be attended by expert eid 
+                # which_k: for each token, which k slot it is from 0 to K 
+                tok_ids, which_k = torch.where(k_ids == eid)
 
-        # count number of tokens for each expert, 
-        ntok = torch.bincount(sorted_expanded_ids, minlength=self.num_experts)
-        
-        # compute offsets to slice permuted token tensor
-        offs = ntok.cumsum(dim=0).to(torch.int32)
+                if expert_capacity and tok_ids.numel() > expert_capacity:
+                    # = Dropping path ===
+                    self.n_drop += int(tok_ids.numel() - expert_capacity)
+                    # sorted=False is critical for order preservation
+                    if self.lb_gamma > 0.0:
+                        _, keep_i = torch.topk(biased_scores[tok_ids, which_k], k=expert_capacity, largest=True, sorted=False)
+                    else:
+                        _, keep_i = torch.topk(router_scores[tok_ids, which_k], k=expert_capacity, largest=True, sorted=False)
+                    
+                    keep_tok_ids = tok_ids[keep_i]
+                    keep_which_k = which_k[keep_i]
+                    tok_ids_by_e.append(keep_tok_ids)
+                    which_k_by_e.append(keep_which_k)
+                else:
+                    tok_ids_by_e.append(tok_ids)
+                    which_k_by_e.append(which_k)
 
-        # dispatch token ids for look up (gather)
-        expanded_tok_ids_to_sorted = expanded_ids_to_sorted // K # to correspond real token ids
-        
-        # = expert compute ================
-        expert_out = self.experts(_x[expanded_tok_ids_to_sorted], offs).to(x.dtype) # x.dtype is still fp32, only autocast as Func, expert accumulation in fp32 is good 
+            ntok = torch.tensor(
+                list(map(lambda x: x.shape[0], tok_ids_by_e)),
+                device=x.device, dtype=torch.int32)
 
-        # expert_out = torch.zeros(T*K, D, device=x.device, dtype=x.dtype) 
-        # for eid in range(E):
-        #     s, e = offs[eid-1] if eid > 0 else 0, offs[eid]
-        #     # slice the lookup token ids, gather input tokens for current expert, forward through expert
-        #     expert_out[s:e] = self.experts[eid]( _x[expanded_tok_ids_to_sorted[s:e]] ) # the same as .index_select(sort_ids[s:e], dim=0)
+            offs = ntok.cumsum(dim=0).to(torch.int32)
 
-        # = combine ================
-        moe_outputs = torch.zeros(T, D, device=x.device, dtype=x.dtype) 
-        # get corresponding weight for token in current permuted order, unsqueeze for broadcasting to d dim later
-        permuted_weights = k_weights.view(-1)[expanded_ids_to_sorted].unsqueeze(-1)
+            expanded_tok_ids = torch.cat(tok_ids_by_e)
+            expanded_which_k = torch.cat(which_k_by_e)
 
-        expert_out *= permuted_weights 
+            # = expert compute ================
+            expert_out = self.experts(_x[expanded_tok_ids], offs).to(x.dtype)
 
-        # why we need this granular mapping? because of how scatter add work, 
-        # it is element-wise mapping
-        expanded_indices = expanded_tok_ids_to_sorted.unsqueeze(-1).expand(-1, D)
-        # expanded_indices [T*K, D] 
-        # scatter_add_(dim, index, src) 
-        # dim=0, meaning moe_outputs[expanded_indices[i]][j] += expert_out[i, j]
-        # expanded_indices has duplicated token ids, accumulation happens
-        moe_outputs.scatter_add_(0, expanded_indices, expert_out)
+            # = combine ================
+            moe_outputs = torch.zeros(T, D, device=x.device, dtype=x.dtype) 
+            # get corresponding weight for token in current permuted order, unsqueeze for broadcasting to d dim later
+            permuted_weights = k_weights[expanded_tok_ids, expanded_which_k].unsqueeze(-1)
 
-        # == Expert Computation and Accumulation ==
-        # initialize output buffer, per token, output dimension
-        # accumulate directly for every k slot.
-        # accumulation save buffer space.
+            expert_out *= permuted_weights 
 
-        # loop over experts
-        # for eid in range(E):
-        #     # tok_ids: indices of tokens to be attended by expert eid 
-        #     # which_k: for each token, which k slot it is from 0 to K 
-        #     tok_ids, which_k = torch.where(k_ids == eid)
+            expanded_indices = expanded_tok_ids.unsqueeze(-1).expand(-1, D)
 
-        #     if expert_capacity and tok_ids.numel() > expert_capacity:
-        #         # = Dropping path ===
-        #         # sorted=False is critical for order preservation
-        #         if self.lb_gamma > 0.0:
-        #             _, keep_i = torch.topk(biased_scores[tok_ids, which_k], k=expert_capacity, largest=True, sorted=False)
-        #         else:
-        #             _, keep_i = torch.topk(router_scores[tok_ids, which_k], k=expert_capacity, largest=True, sorted=False)
-                
-        #         keep_tok_ids = tok_ids[keep_i]
-        #         keep_which_k = which_k[keep_i]
+            moe_outputs.scatter_add_(0, expanded_indices, expert_out)
 
-        #         expert = self.experts[eid]
-        #         tokens = _x[keep_tok_ids]
-        #         weights = k_weights[keep_tok_ids, keep_which_k].unsqueeze(-1)
+        else:
+            # = dispatch ================
+            # k_ids [T, K] contextually token to k-activated expert ids
+            expanded_k_ids = k_ids.view(-1) # k-expanded for ease of understanding, it is flat too
+            
+            # torch.sort returns sorted values and indices to original positions
+            sorted_expanded_ids, expanded_ids_to_sorted = torch.sort(expanded_k_ids, stable=True)
 
-        #         moe_outputs[keep_tok_ids, :] += expert(tokens) * weights 
+            # count number of tokens for each expert, 
+            ntok = torch.bincount(sorted_expanded_ids, minlength=self.num_experts)
+            
+            # compute offsets to slice permuted token tensor
+            offs = ntok.cumsum(dim=0).to(torch.int32)
 
-        #         self.n_drop += int(tok_ids.numel() - expert_capacity)
-        #     else:
-        #         # = Dropless path ===
-        #         # current gather tokens, extract corresponding weights
-        #         expert = self.experts[eid]
-        #         tokens = _x[tok_ids]
-        #         weights = k_weights[tok_ids, which_k].unsqueeze(-1) 
-        #         # forward pass, output accumulate back to moe_outputs
-        #         moe_outputs[tok_ids, :] += expert(tokens) * weights
+            # dispatch token ids for look up (gather)
+            expanded_tok_ids_to_sorted = expanded_ids_to_sorted // K # to correspond real token ids
+            
+            # = expert compute ================
+            expert_out = self.experts(_x[expanded_tok_ids_to_sorted], offs).to(x.dtype) # x.dtype is still fp32, only autocast as Func, expert accumulation in fp32 is good 
 
+            # expert_out = torch.zeros(T*K, D, device=x.device, dtype=x.dtype) 
+            # for eid in range(E):
+            #     s, e = offs[eid-1] if eid > 0 else 0, offs[eid]
+            #     # slice the lookup token ids, gather input tokens for current expert, forward through expert
+            #     expert_out[s:e] = self.experts[eid]( _x[expanded_tok_ids_to_sorted[s:e]] ) # the same as .index_select(sort_ids[s:e], dim=0)
+
+            # = combine ================
+            moe_outputs = torch.zeros(T, D, device=x.device, dtype=x.dtype) 
+            # get corresponding weight for token in current permuted order, unsqueeze for broadcasting to d dim later
+            permuted_weights = k_weights.view(-1)[expanded_ids_to_sorted].unsqueeze(-1)
+
+            expert_out *= permuted_weights 
+
+            # why we need this granular mapping? because of how scatter add work, 
+            # it is element-wise mapping
+            expanded_indices = expanded_tok_ids_to_sorted.unsqueeze(-1).expand(-1, D)
+            # expanded_indices [T*K, D] 
+            # scatter_add_(dim, index, src) 
+            # dim=0, meaning moe_outputs[expanded_indices[i]][j] += expert_out[i, j]
+            # expanded_indices has duplicated token ids, accumulation happens
+            moe_outputs.scatter_add_(0, expanded_indices, expert_out)
+
+        # = shared experts ================
         # loop over shared experts
         for sid in range(self.num_shared_experts):
             # all tokens go to shared experts
